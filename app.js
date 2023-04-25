4ff3c9777b36cd6918842b6d3985f8f36da956b2
Mark-david <ojukwudavid59@outlook.com>
2023-04-25 22:45:31 +0100

    __shopify-clone__

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ video.addEventListener('click', function () {
     playPauseButton.style.backgroundImage = 'url(img/pause-solid.svg)';
   } else {
     video.pause();
-    playPauseButton.style.backgroundImage = 'url(img/play-solid.svg)';
+    playPauseButton.style.backgroundImage = 'url(img/play-button.png)';
   }
 });
 
@@ -54,14 +54,75 @@ playPauseButton.addEventListener('click', function () {
     playPauseButton.style.backgroundImage = 'url(img/pause-solid.svg)';
   } else {
     video.pause();
-    playPauseButton.style.backgroundImage = 'url(img/play-solid.svg)';
+    playPauseButton.style.backgroundImage = 'url(img/play-button.png)';
   }
 });
 
 video.addEventListener('play', function () {
-  playPauseButton.style.display = 'none';
+  playPauseButton.style.visibility = 'hidden';
 });
 
 video.addEventListener('pause', function () {
-  playPauseButton.style.display = 'block';
+  playPauseButton.style.visibility = 'visible';
 });
+
+// // Get the canvas element
+// const canvas = document.getElementById('canvas');
+
+// // Set the canvas dimensions
+// canvas.width = 800;
+// canvas.height = 600;
+
+// // Load the image of the earth
+// const earth_image = new Image();
+// earth_image.src = 'img/earth.png';
+// earth_image.onload = function () {
+//   // Draw the earth image on the canvas once it has loaded
+//   canvas
+//     .getContext('2d')
+//     .drawImage(earth_image, 0, 0, canvas.width, canvas.height);
+// };
+
+// // Set the initial position and velocity of the orbiting line
+// let line_pos = [400, 300];
+// let line_vel = [Math.random() * 2 - 1, Math.random() * 2 - 1];
+
+// // Set up the animation loop
+// function animate() {
+//   // Update the position of the orbiting line
+//   line_pos[0] += line_vel[0];
+//   line_pos[1] += line_vel[1];
+
+//   // Check if the line intersects with the earth
+//   if (
+//     line_pos[0] > 0 &&
+//     line_pos[0] < 800 &&
+//     line_pos[1] > 0 &&
+//     line_pos[1] < 600
+//   ) {
+//     const earth_data = canvas
+//       .getContext('2d')
+//       .getImageData(line_pos[0], line_pos[1], 1, 1).data;
+//     if (earth_data[0] === 0 && earth_data[1] === 0 && earth_data[2] === 255) {
+//       line_vel = [Math.random() * 2 - 1, Math.random() * 2 - 1];
+//     }
+//   }
+
+//   // Clear the canvas
+//   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+//   // Draw the earth and the orbiting line
+//   canvas
+//     .getContext('2d')
+//     .drawImage(earth_image, 0, 0, canvas.width, canvas.height);
+//   canvas.getContext('2d').beginPath();
+//   canvas.getContext('2d').moveTo(line_pos[0], line_pos[1]);
+//   canvas.getContext('2d').lineTo(400, 300);
+//   canvas.getContext('2d').stroke();
+
+//   // Repeat the animation loop
+//   requestAnimationFrame(animate);
+// }
+
+// // Start the animation loop
+// animate();
